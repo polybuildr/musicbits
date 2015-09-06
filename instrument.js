94a300961a6ebc3ed17c71593b0a7a2783f40ddd
Vivek Ghaisas <v.a.ghaisas@gmail.com>
2015-09-07 01:08:55 +0530

    Refactor instrument.js, make Piano and Flute classes

--- instrument.js
+++ instrument.js
@@ -2,60 +2,70 @@ var Note = require('./note.js')
     , EventEmitter = require('events').EventEmitter
     , util = require('util')
 
-function Instrument(melody, adsrConfig, fullNoteDuration) {
-    EventEmitter.call(this)
-    function frequency(key) {
-        return Math.pow(2, (key - 49) / 12) * 440
-    }
-
-    var pianoConfig = {
+function Piano(fullNoteDuration) {
+    fullNoteDuration = fullNoteDuration || 0.3
+    var adsrConfig = {
         a: 0.01,
         d: 0.5,
         s: 0,
         r: 0.2
     }
+    Instrument.call(this, adsrConfig, fullNoteDuration)
+}
 
-    var fluteConfig = {
+function Flute(fullNoteDuration) {
+    fullNoteDuration = fullNoteDuration || 0.3
+
+    var adsrConfig = {
         a: 0.1,
         d: 0.1,
         s: 1,
         r: 0.05
     }
 
-    if (adsrConfig == 'piano') {
-        adsrConfig = pianoConfig
-    }
-    else if (adsrConfig == 'flute') {
-        adsrConfig = fluteConfig
-    }
-    else {
-        adsrConfig = pianoConfig
-    }
-    fullNoteDuration = fullNoteDuration || 0.3
+    Instrument.call(this, adsrConfig, fullNoteDuration)
+}
 
-    var notes = [];
+function Instrument(adsrConfig, fullNoteDuration) {
+    EventEmitter.call(this)
 
-    for (var i = 0; i < melody.length; i++) {
-        var note = melody[i]
-
-        var freq = frequency(note.key)
-        var duration = (note.duration || 1) * fullNoteDuration
-
-        if (note.sustain) {
-            var sustainADSRConfig = {
-                a: adsrConfig.a,
-                d: adsrConfig.d,
-                s: adsrConfig.s,
-                r: adsrConfig.r * 5
-            }
-            notes.push(new Note(freq, duration, sustainADSRConfig))
-        }
-        else {
-            notes.push(new Note(freq, duration, adsrConfig))
-        }
+    function frequency(key) {
+        return Math.pow(2, (key - 49) / 12) * 440
     }
+
+    var playing = false
     var playingIndex = 0
+    var notes = []
+
+    this.play = function(melody) {
+        notes = [];
+
+        for (var i = 0; i < melody.length; i++) {
+            var note = melody[i]
+
+            var freq = frequency(note.key)
+            var duration = (note.duration || 1) * fullNoteDuration
+
+            if (note.sustain) {
+                var sustainADSRConfig = {
+                    a: adsrConfig.a,
+                    d: adsrConfig.d,
+                    s: adsrConfig.s,
+                    r: adsrConfig.r * 5
+                }
+                notes.push(new Note(freq, duration, sustainADSRConfig))
+            }
+            else {
+                notes.push(new Note(freq, duration, adsrConfig))
+            }
+        }
+        playing = true
+    }
+
     this.value = function(t) {
+        if (!playing) {
+            throw new Error('Instrument not playing.')
+        }
         if (playingIndex < notes.length) {
             if (notes[playingIndex].wasAttacked() == false) {
                 notes[playingIndex].play(t)
@@ -76,5 +86,8 @@ function Instrument(melody, adsrConfig, fullNoteDuration) {
 }
 
 util.inherits(Instrument, EventEmitter)
+util.inherits(Piano, Instrument)
+util.inherits(Flute, Instrument)
 
-module.exports = Instrument
+module.exports.Instrument = Instrument
+module.exports.Piano = Piano
